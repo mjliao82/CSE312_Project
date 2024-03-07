c3577b8eb150285be2660d55b29dae67891c6e90
rar38 <rar38@buffalo.edu>
2024-03-06 23:53:46 -0500

    - Made small fixes to app.py including some typo errors - Implemented registering and logging in - Implemented authenticating logging in and generating tokens, see authenticity.py for the database details.

--- authenticity.py
+++ authenticity.py
@@ -1,9 +1,63 @@
-#ray's file
+# ray's file
+import hashlib
+import html
+import secrets
+
+import bcrypt
+from pymongo import MongoClient
+
+mongo_client = MongoClient("mongo")
+db = mongo_client["cse312_group"]
+user_collection = db["users"]
+token_collection = db["tokens"]
 
 
-def takethis(username, password):
+def user_registration(username, password, confirm_password):
+    valid = check_password(password)
+    username_fixed = html.escape(username)
+    if valid and password == confirm_password:
+        salt = bcrypt.gensalt()
+        hashed_passwd = bcrypt.hashpw(password.encode("utf-8"), salt)
+        user_collection.insert_one({'username': username_fixed, 'password': hashed_passwd, 'salt': salt})
     return
 
 
-def takethat(username, password):
-    return
+def user_login(username, password):
+    user_info = user_collection.find_one({'username': username})
+    if user_info:
+        salt = user_info['salt'].decode('utf-8')
+        hashed_passwd = bcrypt.hashpw(password.encode("utf-8"), salt.encode("utf-8"))
+        if hashed_passwd == user_info['password']:
+            token = secrets.token_urlsafe(80)
+            sha256 = hashlib.sha256()
+            sha256.update(token.encode("utf-8"))
+            token_collection.insert_one({'username': username, 'token': sha256.hexdigest()})
+            return [True, token]
+    return [False, '']
+
+
+# Helper Functions
+
+def check_password(password):
+    uppercase = False
+    lowercase = False
+    valid_length = False
+    special_char = False
+    contains_num = False
+    not_invalid = True
+
+    if len(password) >= 8:
+        valid_length = True
+
+    for char in password:
+        if char.islower():
+            lowercase = True
+        elif char.isupper():
+            uppercase = True
+        elif char.isdigit():
+            contains_num = True
+        elif char in '!@#$%^&()-_=':
+            special_char = True
+        else:
+            not_invalid = False
+    return lowercase and uppercase and contains_num and special_char and valid_length and not_invalid
