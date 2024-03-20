eef9e15f30f8f88808e0f9669e56bcd225537bb7
mjliao82 <mjliao82@gmail.com>
2024-03-20 19:01:11 -0400

    js post request

--- static/home.js
+++ static/home.js
@@ -16,11 +16,27 @@ function display() {
     request.send();
 }
 
-// function fetchData() {
-//     fetch('/login')
-//         .then(response => response.json())
-//         .then(data => {
-//             document.getElementById('data').textContent = JSON.stringify(data);
-//         })
-//         .catch(error => console.error('Error fetching data:', error));
-// }
+function sendChat() {
+    const chatTextBox = document.getElementById("chat-text-box");
+    const message = chatTextBox.value;
+    const hiddenXSRF = document.getElementById('xsrf');
+    const token = hiddenXSRF.innerText;
+    chatTextBox.value = "";
+    //hiddenXSRF.value = "{{XSRF}}";
+    if (ws) {
+        // Using WebSockets
+        socket.send(JSON.stringify({'messageType': 'chatMessage', 'message': message, 'token':token}));
+    } else {
+        // Using AJAX
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log(this.response);
+            }
+        }
+        const messageJSON = {"message": message, "token":token};
+        request.open("POST", "/chat-messages");
+        request.send(JSON.stringify(messageJSON));
+    }
+    chatTextBox.focus();
+}
