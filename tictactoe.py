337a82c1ff46489249268058575a71c4540905e7
rar38 <rar38@buffalo.edu>
2024-04-19 17:21:15 -0400

    Changed filename

--- tictactoe.py
+++ tictactoe.py
@@ -4,7 +4,6 @@ from authenticity import db, findingUser
 
 turn_order = db["turn_order"]
 game_boards = db["game_boards"]
-game_queue = db["game_queue"]
 
 places = {"1": (0, 0), "2": (0, 1), "3": (0, 2), "4": (1, 0), "5": (1, 1), "6": (1, 2), "7": (2, 0), "8": (2, 1),
           "9": (2, 2)}
